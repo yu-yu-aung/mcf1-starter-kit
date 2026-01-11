18f525d024b415130d849fcff369c2085992eeca
yu-yu-aung <yuyuaungayy@gmail.com>
2026-01-11 17:59:02 +0630

    fix: folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
+import "../style/globals.css";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
